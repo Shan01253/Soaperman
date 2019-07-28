472a621e99e390bc92d2a92d792df5aa771bd577
YahtzeeRage <34356761+YahtzeeRage@users.noreply.github.com>
2019-07-28 16:49:15 -0400

    r

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    float horizontalInput;
+    float verticalInput;
+    Rigidbody2D rb;
+
+    public float speed = 10;
+    public float smoothFactor = .2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = speed * Input.GetAxisRaw("Horizontal");
+        verticalInput = speed * Input.GetAxisRaw("Vertical");
+
+        rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(horizontalInput, verticalInput), smoothFactor);
+    }
+}
