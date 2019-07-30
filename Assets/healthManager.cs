3c216c69ea55869ae77749ee4a4a16c0a6e29108
YahtzeeRage <34356761+YahtzeeRage@users.noreply.github.com>
2019-07-30 18:09:58 -0400

    added healthmanager to soaperman

---
+++ Assets/healthManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class healthManager : MonoBehaviour
+{
+    public float currentHealthPercent = 1;
+    public float remainingHearts = 3;
+
+    public void Damage(float percentAmount)
+    {
+        if (currentHealthPercent - percentAmount < 0)
+        {
+            if (remainingHearts == 0)
+            {
+                Die();
+            }
+            else
+            {
+                currentHealthPercent = 1;
+                remainingHearts--;
+            }
+
+        }
+        else
+        {
+            currentHealthPercent -= percentAmount;
+        }
+    }
+
+    public void Die()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
